656d8a89b6164d8cbc34f8f766c8b8a6e4295c4b
AryanNoSensie <offciearyan14@gail.com>
2025-02-20 16:49:16 +0530

    reacoil

--- week12/contextapi/src/App.jsx
+++ week12/contextapi/src/App.jsx
@@ -1,29 +1,59 @@
- 
-function App(){
-  return(
-    <div>
-    <Lightbulb/>
-    </div>
-  )
-}
 
-function Lightbulb(){
 
-  return(
-    <div>
-     <Bulbstate/>
-     <Toglestate/>
-    </div>
-  )
-}
-
-function Bulbstate(){
-return (
+import {RecoilRoot  ,atom, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { counterAtom } from './store/atoms/counter';
+function App() {
+  
+   return(
+   <>
+   <RecoilRoot>
+    <Counter/>
+   </RecoilRoot>
    
-)
+   
+   </>
+   )
 }
 
-function Toglestate(){
-
+function Counter(){
+  return(
+    <div>
+      <Counts/>
+      <Increase  />
+      <Decrease  />
+    </div>
+  )
 }
+function Counts(){
+  const count = useRecoilValue(counterAtom)
+  return(<div>
+    {count}
+  </div>
+  )
+}
+function Increase(){
+
+  const setCount = useSetRecoilState(counterAtom)
+  function increase(){
+    setCount(c=>c+1);
+  }
+
+ return(<div>
+  <button onClick={increase}>increase</button>
+  </div>
+ )
+ }
+ function Decrease(){
+  const setCount = useSetRecoilState(counterAtom)
+  function decrease(){
+    setCount(c=>c-1);
+  }
+  return(<div>
+
+   <button onClick={decrease}>decrease</button>
+   </div>
+  )
+  }
+
+
 export default App;
